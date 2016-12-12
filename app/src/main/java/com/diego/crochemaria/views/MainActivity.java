fd925c16c38fc53213709c63966d0213b489b04f
Diego <di3g0d0ming05@gmail.com>
2016-12-12 16:58:24 -0200

    fonts

--- app/src/main/java/com/diego/crochemaria/views/MainActivity.java
+++ app/src/main/java/com/diego/crochemaria/views/MainActivity.java
@@ -1,8 +1,10 @@
 package com.diego.crochemaria.views;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 import com.diego.crochemaria.R;
@@ -10,6 +12,8 @@ import com.diego.crochemaria.adapters.ProdutoAdpter;
 import com.diego.crochemaria.endpoint.EndPoint;
 import com.diego.crochemaria.models.Produto;
 import com.diego.crochemaria.utils.ConfigRetrofit;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -20,31 +24,41 @@ import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView produtos;
+//    private ListView produtos;
+        private TextView teste;
+    final int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface font = Typeface.createFromAsset(getAssets(),"Pacifico-Regular.ttf");
+        TextView titulo = (TextView) findViewById(titleId);
+        titulo.setTypeface(font);
+        this.setTitle(titulo.toString());
 
-        produtos = (ListView) findViewById(R.id.list_prod);
 
-        Retrofit retrofit = ConfigRetrofit.conexao();
-        EndPoint api = retrofit.create(EndPoint.class);
-        Call<List<Produto>> call = api.produtos();
-        call.enqueue(new Callback<List<Produto>>() {
-            @Override
-            public void onResponse(Response<List<Produto>> response, Retrofit retrofit) {
-                final ProdutoAdpter adpter = new ProdutoAdpter(getBaseContext(), response.body());
-                produtos.setAdapter(adpter);
-            }
 
-            @Override
-            public void onFailure(Throwable t) {
 
-            }
-        });
+
+//        produtos = (ListView) findViewById(R.id.list_prod);
+//
+//        Retrofit retrofit = ConfigRetrofit.conexao();
+//        EndPoint api = retrofit.create(EndPoint.class);
+//        Call<List<Produto>> call = api.produtos();
+//        call.enqueue(new Callback<List<Produto>>() {
+//            @Override
+//            public void onResponse(Response<List<Produto>> response, Retrofit retrofit) {
+//                final ProdutoAdpter adpter = new ProdutoAdpter(getBaseContext(), response.body());
+//                produtos.setAdapter(adpter);
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+//
+//            }
+//        });
 
     }
 }
